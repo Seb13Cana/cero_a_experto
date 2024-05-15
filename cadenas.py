b7718a6ff90f3bee811ea0f6808d12e30a1c7d00
Seb13Cana <67483247+Seb13Cana@users.noreply.github.com>
2024-05-15 15:08:45 -0500

    Project initialization

---
+++ cadenas.py
@@ -0,0 +1,17 @@
+# MAnejo de cadenas
+
+mi_cadena = 'Sebastian Cañadulce'
+
+print(mi_cadena)
+
+#Agregar salto de linea
+
+mi_cadena= 'Sebastian \nCañadulce'
+
+print(mi_cadena)
+#Agregar Tabulador  \t ----
+
+mi_cadena =  '\tSebastian \nCañadulce'
+
+print(mi_cadena)
+
