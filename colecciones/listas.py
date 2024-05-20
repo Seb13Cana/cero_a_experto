cdf30166e63a66af674b35a8ea707ebbbd02c4fb
Seb13Cana <67483247+Seb13Cana@users.noreply.github.com>
2024-05-20 16:37:12 -0500

    Añadir colecciones y listas.py

---
+++ colecciones/listas.py
@@ -0,0 +1,60 @@
+print('***Listas en python ***')
+nombres = ['Lucas', 'Pedro', 'Thomas']
+
+print(nombres)
+
+for nombre in nombres:
+    print(f'Los nombres de los discipulos son: {nombre}')
+
+#Las listas pueden ser heterogeneas
+
+lista_heterogenea = [250, True, 'Marta', 256.35]
+
+print('Datos lista heterogenea')
+for item in lista_heterogenea:
+    print(item)
+
+#Recuperar elemento de la lista
+numeros = [1,2,3,4,5,6]
+print(numeros[1])
+
+#Modificar elementos
+#print(numeros[2])
+#numeros[2] = 8
+#print(numeros[2])
+
+#Buscar elementos
+numero_a_buscar = 5
+
+if numero_a_buscar in numeros:
+    print(f'si se encuentra el numero {numero_a_buscar} en la lista')
+else:
+    print(f'no se encuentra el numero: {numero_a_buscar} en la lista')
+
+#Recuperar indices
+indice = numeros.index(numero_a_buscar)
+
+print(f'El indice de {numero_a_buscar} en la lista numeros es: {indice}')
+
+#Recuperar tramos de la lista
+numeros = [1,5,2,3,6,22,18,0,11,4]
+numeros_recuperados = numeros[0:4]
+print(numeros_recuperados)
+#Sin indicaf el indice inicial
+numeros_recuperados= numeros[:3]
+print(numeros_recuperados)
+#sin indicar el indice final
+numeros_recuperados = numeros[7:]
+print(numeros_recuperados)
+
+
+#metodos
+# hallar el largo de una lista len
+largo_de_una_lista = len(numeros)
+print(largo_de_una_lista)
+# Agregar item a la lista
+numeros.append(54)
+print(numeros)
+#insertar elementos en el indice deseado
+numeros.insert(4,66)
+print(numeros)
